0dabfacc7ccfdcb9f1ad968c78d0c7331baa2599
Noelle Laureano <nrlaureano@gmail.com>
2021-04-25 17:07:43 -0400

    Updates logs in readFile

--- readFile.js
+++ readFile.js
@@ -7,7 +7,10 @@ fs.readFile('./readMe.csv', 'utf8', (err, data) => {
   if (data) {
     console.log('Line 8  >> File contains…');
     console.log(chalk.yellow(data));
-  } else console.error('Line 7  >> err:', err);
+  } else {
+    console.error('Line 11 >> Error!');
+    console.log(chalk.red(err));
+  }
 });
 
-console.log('Line 10 >> Moving on…');
+console.log('Line 16 >> Moving on…');
