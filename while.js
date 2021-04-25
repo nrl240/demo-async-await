e5882ea879d1821457a5c87e8539a0293c241b75
Noelle Laureano <nrlaureano@gmail.com>
2021-04-25 16:50:43 -0400

    Updates while.js console logs

--- while.js
+++ while.js
@@ -1,18 +1,18 @@
 function syncroFunc(num) {
-  console.log('INSIDE: Running syncroFunc...')
+  console.log('Line 2  >> INSIDE:  Running syncroFunc...');
 
-  while(num) {
-    console.log(`INSIDE: ${num}`)
-    num--
+  while (num) {
+    console.log(`Line 5  >> INSIDE:  ${num}`);
+    num--;
   }
 
-  console.log('INSIDE: Stopping syncroFunc...')
+  console.log('Line 9  >> INSIDE:  Stopping syncroFunc...');
 
-  return 'Blast Off!'
+  return 'Blast Off!';
 }
 
-console.log('OUTSIDE: About to run syncroFunc...')
+console.log('Line 14 >> OUTSIDE: About to run syncroFunc...');
 
-let result = syncroFunc(5)
+let result = syncroFunc(5);
 
-console.log(`OUTSIDE: syncroFunc(5) ran; result --> ${result}`)
+console.log(`Line 18 >> OUTSIDE: syncroFunc(5) ran; result --> ${result}`);
